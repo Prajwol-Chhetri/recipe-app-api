ad0e65eb58a81f1eca5381b89c963d7d6600a432
Prajwol Chhetri <prajwalc391@gmail.com>
2021-07-31 22:50:12 +0545

    Fixing Flake8

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,4 +1,3 @@
-from django import urls
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -9,16 +8,17 @@ class AdminSiteTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email = 'admin@test.com',
-            password = 'password123'
+            email='admin@test.com',
+            password='password123'
         )
-        self.client.force_login(self.admin_user)  # helper function to login the user
+        # helper function to login the user
+        self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email = 'user@test.com',
-            password = 'password123',
-            name = 'Test user full name'
+            email='user@test.com',
+            password='password123',
+            name='Test user full name'
         )
-    
+
     def test_users_listed(self):
         """Test that users are listed on user page"""
         url = reverse('admin:core_user_changelist')
@@ -26,7 +26,7 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
-    
+
     def test_user_change_page(self):
         """Test that the user edit page works"""
         url = reverse('admin:core_user_change', args=[self.user.id])
