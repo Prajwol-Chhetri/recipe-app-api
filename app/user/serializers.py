ce47ebdba928d2900504816a9fba37ae144a885c
Prajwol Chhetri <prajwalc391@gmail.com>
2021-08-01 17:01:57 +0545

    Added working list tags endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -48,7 +48,7 @@ class AuthTokenSerializer(serializers.Serializer):
             password=password
         )
         if not user:
-            msg = _('Unable to authenticcate with provided credentials')
+            msg = _('Unable to authenticate with provided credentials')
             raise serializers.ValidationError(msg, code='authentication')
 
         attrs['user'] = user
